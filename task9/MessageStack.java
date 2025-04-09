081c58a786a12ccceed5524e877a2fb4a14b23d7
Elias Jovancic Persson <EliasJovancicPersson@gmail.com>
2025-04-09 11:53:16 +0200

    task9 done

---
+++ task9/MessageStack.java
@@ -0,0 +1,39 @@
+package task9;
+
+import java.util.ArrayList;
+
+public class MessageStack {
+
+    ArrayList<Message> elements;
+
+    public MessageStack() {
+        elements = new ArrayList<Message>();
+    }
+
+    public void push(Message message) {
+        this.elements.add(message);
+    }
+
+    public Message pop() {
+        try {
+            return this.elements.remove(this.elements.size() - 1);
+        } catch (Exception e) {
+            System.out.println("Stack is empty");
+            return null;
+        }
+    }
+
+    public void push(int n, Message[] messages) {
+        for (int i = 0; i < n; i++) {
+            this.push(messages[i]);
+        }
+    }
+
+    public Message[] pop(int n) {
+        Message[] messages = new Message[n];
+        for (int i = 0; i < n; i++) {
+            messages[i] = this.pop();
+        }
+        return messages;
+    }
+}
---
+++ task9/MessageStackTest.java
@@ -0,0 +1,35 @@
+package task9;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+
+
+public class MessageStackTest {
+    Message testMessage = new Message("Hello1");
+    Message testMessage1 = new Message("Hello2");
+    Message testMessage2 = new Message("Hello3");
+
+
+     @org.junit.Test
+    public void testPush() {
+        MessageStack stack = new MessageStack();
+        stack.push(testMessage);
+        stack.push(testMessage1);
+        stack.push(testMessage2);
+        assertEquals(testMessage2, stack.pop());
+        assertEquals(testMessage1, stack.pop());
+        assertEquals(testMessage, stack.pop());
+    }
+
+    @org.junit.Test
+    public void testPushMany(){
+        MessageStack stack = new MessageStack();
+        Message[] input = new Message[]{testMessage,testMessage1,testMessage2};
+        stack.push(3, input);
+        Message[] Output = stack.pop(3);
+        System.out.println(Arrays.toString(Output)); // Print the array contents
+        assertArrayEquals(new Message[]{testMessage2,testMessage1,testMessage}, Output);
+    }
+}
